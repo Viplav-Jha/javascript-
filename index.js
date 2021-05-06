655894bc39a46f81cfffb801047404e7a0063196
Viplav Jha <67279519+Viplav-Jha@users.noreply.github.com>
2021-05-06 19:29:04 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+/**** Section 3👉 values and variables in JavaScript ****/
+
+var myName = "viplav kumar jha"
+var myage =24;
+
+console.log(myage);
+
+
+// Naming Practice
+
+// var _myName ="viplav";
+// var 1myNmae="viplav"; // Not valid in Javascript
+// var _my_Name ="viplav;"
+// var $myName ="viplav"
+// var myNaem% = "viplav";
+// console.log(myNaem%);
+
+
+/**** Section 4👉 Data Types in JavaScript ****/
+
+
+// var myName = "Viplav Jha";
+// console.log(myName);
+
+// var myAge = 24;
+// console.log(myAge);
+
+// var iamViplav=false;
+// console.log(iamViplav);
+// console.log(typeof(iamViplav));
+
+// DataTypes Practice
+console.log(10+"20");
+console.log(10-"5"); //bug
+console.log("java"+"script");
+"" + ""
+console.log("" +0);
+console.log("viplav"-"jha"); //NaN
